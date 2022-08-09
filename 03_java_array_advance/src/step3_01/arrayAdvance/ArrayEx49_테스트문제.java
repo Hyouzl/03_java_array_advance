68891164c4d45e5ce138215bb8c7d4b11a856619
yuri-note <yuri-note@LAPTOP-NI438OED>
2022-08-09 18:20:16 +0900

    문제품

--- 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx49_테스트문제.java
+++ 03_java_array_advance/src/step3_01/arrayAdvance/ArrayEx49_테스트문제.java
@@ -1,5 +1,7 @@
 package step3_01.arrayAdvance;
 
+import java.util.Scanner;
+
 /*
 문제1) 숫자 0이 플레이어이다. 
 	    
@@ -30,6 +32,99 @@ public class ArrayEx49_테스트문제 {
 				};
 		
 		int yx [][] = new int[10000][2];
+		int player;
+		int px = 0;
+		int py = 0;
+		int temp = 0;
+		int cnt = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while (true) {
+			
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (game[i][j] == 0) {
+						px = j;
+						py = i;
+					}
+				}
+			}
+		player = game[py][px];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print(game[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("1) left 2)right 3)up 4)down 5)되감기");
+		System.out.println("번호 입력 : ");
+		int move = scan.nextInt();
+		
+		if (move == 1) {
+			if (px == 0) {
+				System.out.println("이동 불가");
+			}
+			else {
+				temp = game[py][px-1];	
+				game[py][px-1] = player;
+				game[py][px] = temp;
+				yx[cnt][0] = px;
+				yx[cnt][1] = py;
+				cnt++;
+ 			}
+		}
+		else if (move == 2) {
+			if (px == 3) {
+				System.out.println("이동 불가");
+			}
+			else {
+				temp = game[py][px+1];
+				game[py][px+1] = player;
+				game[py][px] = temp;
+				yx[cnt][0] = px;
+				yx[cnt][1] = py;
+				cnt++;
+			}
+		}
+		
+		else if (move == 3) {
+			if (py == 0) {
+				System.out.println("이동 불가");
+			}
+			else {
+				temp = game[py-1][px];
+				game[py-1][px] = player;
+				game[py][px] = temp;
+				yx[cnt][0] = px;
+				yx[cnt][1] = py;
+				cnt++;
+			}
+		}
+		else if (move == 4) {
+			if (py == 3) {
+				System.out.println("이동 불가");
+			}
+			else {
+				temp = game[py+1][px];
+				game[py+1][px] = player;
+				game[py][px] = temp;
+				yx[cnt][0] = px;
+				yx[cnt][1] = py;
+				cnt++;
+			}
+		}
+		
+		else if (move == 5) {
+			int rtPx = yx[cnt-1][0];
+			int rtPy = yx[cnt-1][1];
+			game[rtPy][rtPx] = player;
+			game[py][px] = temp;
+		}
+		
+		}
+		
 
 	}
 
